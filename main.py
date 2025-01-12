027b2a0613fee279184d79eeb266e8426f89f809
WeronikaD06 <weronikaxduszka@gmail.com>
2025-01-12 22:49:46 +0100

    implemented bmi_input

--- main.py
+++ main.py
@@ -24,8 +24,29 @@ def bmi_text():
     return 0
 
 def bmi_input():
-   #bmi input
-    return  0
+    waga = int(input("Ile ważysz? (podaj w kilogramach): "))
+    wzrost = float(input("Ile masz wzrostu? (podaj w metrach)"))
+    bmi = waga / (wzrost ** 2)
+    print(f"Twoje BMI wynosi: {format(bmi, '.2f')}")
+    print("Twoja diagnoza: ")
+    if bmi <= 0:
+        print("Błąd. Sprawdź jeszcze raz!")
+    elif bmi > 0 and bmi < 16:
+        print("wygłodzenie")
+    elif bmi >= 16 and bmi < 17:
+        print("wychudzenie")
+    elif bmi >= 17 and bmi < 18.5:
+        print("niedowaga")
+    elif bmi >= 18.5 and bmi < 25:
+        print("pożądana masa ciała")
+    elif bmi >= 25 and bmi < 30:
+        print("nadwaga")
+    elif bmi >= 30 and bmi < 35:
+        print("otyłość 1 stopnia")
+    elif bmi >= 35 and bmi < 40:
+        print("otyłość 2 stopnia")
+    else:
+        print("otyłość 3 stopnia")
 
 def bmi_charts():
     #bmi charts
